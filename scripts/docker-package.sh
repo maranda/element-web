84b4c25a8329a113e3a35f2b8ff6076dd946b311
Marco Cirillo <marco.cirillo@aria-net.org>
2023-12-04 23:37:46 +0100

    Update version to 1.11.50

--- scripts/docker-package.sh
+++ scripts/docker-package.sh
@@ -3,7 +3,7 @@
 set -ex
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-DIST_VERSION=v1.11.49 # git describe just breaks in Alpine
+DIST_VERSION=v1.11.50 # git describe just breaks in Alpine
 
 DIR=$(dirname "$0")
 
