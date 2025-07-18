a5119c5dd32c2260803791201c8e2e57239ddd0c
Marco Cirillo <marco.cirillo@aria-net.org>
2025-07-18 13:09:03 +0200

    Make docker package executable

--- scripts/docker-package.sh
+++ scripts/docker-package.sh
@@ -3,7 +3,7 @@
 set -ex
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-DIST_VERSION=v1.11.106 # git describe just breaks in Alpine
+DIST_VERSION=v1.11.106 # git describe just breaks in Alpine -- make executable
 
 DIR=$(dirname "$0")
 
